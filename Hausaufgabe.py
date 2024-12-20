63e031c3b62e69cbf2d4b4096c049ee235c09901
stsi-ws24-12 <phammy98431@th-nuernberg.de>
2024-12-20 15:45:59 +0100

    Update Hausaufgabe.py

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -5,9 +5,9 @@ def dumme_addition(a:int, b:int):
     if a == 0:
         return b
     if b ==0:
-        return a+1
+        return a
     else:
-        return dumme_addition(a - 1, b - 1) + 2
+        return 1 + dumme_addition(a, b - 1) 
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
