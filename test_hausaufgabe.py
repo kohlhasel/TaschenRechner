28f0278731e5fff6532b993c5399fa67338c4868
hassella85754 <hassella85754@th-nuernberg.de>
2024-12-21 18:44:06 +0100

    Update test_hausaufgabe.py

--- test_hausaufgabe.py
+++ test_hausaufgabe.py
@@ -15,7 +15,21 @@ class TestAufgabeEins():
         # Assert
         assert result == 30
 
-
+    def test_multiplikation_mit_null():
+        '''Multiplikation mit 0 ergibt immer 0'''
+        # Arrange
+        input1 = 0
+        input2 = 5
+        # Act
+        result = dumme_multiplikation(input1, input2)
+        # Assert
+        assert result == 0
+    
+        # Test für andere Reihenfolge der Argumente
+        input1 = 5
+        input2 = 0
+        result = dumme_multiplikation(input1, input2)
+        assert result == 0
 
     def test_assoziativ(self):
         '''Multiplikation ist assoziativ <==> (a*b)*c= a*(b*c)'''
