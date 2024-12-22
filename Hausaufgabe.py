73d3dbc8dd9953eb50e8a13ae3b86dc20f19b18c
JTMoeHub <jannik-moeller@gmx.de>
2024-12-22 12:39:06 +0100

    Update Hausaufgabe.py

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -2,12 +2,17 @@ from typing import List
 
 
 def dumme_multiplikation(a:int, b:int):
+    # Prüfen, ob einer der Werte 0 ist
+    if a == 0 or b == 0:
+        return 0  # Multiplikation mit 0 ergibt immer 0
+
     if a == 1:
         return b
-    if b ==1:
-        return a+1
+    if b == 1:
+        return a
     else:
-        return dumme_multiplikation(a-1,b-1)+a+b-1
+        # Die rekursive Berechnung bleibt unverändert
+        return dumme_multiplikation(a - 1, b - 1) + a + b - 1
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
