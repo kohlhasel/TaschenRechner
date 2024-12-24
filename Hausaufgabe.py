37c7bcafd49e628c399f129cf85aee4cb928b3fb
anika-w <wittmannan94173@th-nuernberg.de>
2024-12-24 14:49:24 +0100

    Update Hausaufgabe.py
    Anpassung groesste_n_zahlen

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 def dumme_multiplikation(a: int, b: int):
     if a == 0 or b == 0:
         return 0
@@ -11,9 +10,8 @@ def dumme_multiplikation(a: int, b: int):
     else:
         return a + dumme_multiplikation(a, b - 1)
 
-
-def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
-    return sorted(liste)[-n:]
+def groesste_n_zahlen_implementation_1(liste: List[int], n: int):
+    return sorted(liste)[-n:][::-1]
 
 def groesste_n_zahlen_implementation_2(liste:List[int], n: int):
     return sorted(liste, reverse=True)[:n]
