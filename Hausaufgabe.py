ed890625f12c6f68ec190843f8b00e07b4cd3c7a
anika-w <wittmannan94173@th-nuernberg.de>
2024-12-27 09:27:39 +0100

    Update Hausaufgabe.py
    Korrektur dumm_multiplikation mit negativen Zahlen

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -1,14 +1,17 @@
 from typing import List
 
 def dumme_multiplikation(a: int, b: int):
-    if a == 0 or b == 0:
+    if a == 0 or b == 0:  # Multiplikation mit 0
         return 0
-    if a == 1:
+    if a == 1:  # 1 * b = b
         return b
-    if b == 1:
+    if b == 1:  # a * 1 = a
         return a
-    else:
-        return a + dumme_multiplikation(a, b - 1)
+    if b < 0:  # Behandlung von negativen b
+        return -dumme_multiplikation(a, -b)
+    if a < 0:  # Behandlung von negativen a
+        return -dumme_multiplikation(-a, b)
+    return a + dumme_multiplikation(a, b - 1)  # Rekursion für normale Multiplikation
 
 def groesste_n_zahlen_implementation_1(liste: List[int], n: int):
     return sorted(liste)[-n:][::-1]
