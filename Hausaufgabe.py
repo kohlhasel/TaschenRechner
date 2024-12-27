b8e2ebb1e1296f27768267efe6c58b7505eb04b5
anika-w <wittmannan94173@th-nuernberg.de>
2024-12-27 09:45:35 +0100

    Update Hausaufgabe.py
    Versuche wieder rückgängig machen

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -7,14 +7,11 @@ def dumme_multiplikation(a: int, b: int):
         return b
     if b == 1:  # a * 1 = a
         return a
-    if b < 0:  # Behandlung von negativen b
-        return -dumme_multiplikation(a, -b)
-    if a < 0:  # Behandlung von negativen a
-        return -dumme_multiplikation(-a, b)
-    return a + dumme_multiplikation(a, b - 1)  # Rekursion für normale Multiplikation
+   else
+        return a + dumme_multiplikation(a, b-1)
 
 def groesste_n_zahlen_implementation_1(liste: List[int], n: int):
-    return sorted(liste)[-n:][::-1]
+    return sorted(liste)[-n:]
 
 def groesste_n_zahlen_implementation_2(liste:List[int], n: int):
     return sorted(liste, reverse=True)[:n]
