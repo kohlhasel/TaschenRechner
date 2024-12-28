328c9a2e5b8a4fdbce6bcd9e9f39e9d5c738e921
Krissy1111 <medererkr80230@th-nuernberg.de>
2024-12-28 14:14:01 +0100

    Update Hausaufgabe.py

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -1,13 +1,19 @@
 from typing import List
 
 
-def dumme_multiplikation(a:int, b:int):
+def dumme_multiplikation(a: int, b: int):
+    # Abbruchbedingungen für 0
+    if a == 0 or b == 0:
+        return 0
+    # Abbruchbedingung für a = 1
     if a == 1:
         return b
+    # Abbruchbedingung für b = 1
     if b == 1:
         return a
+    # Rekursiver Schritt
     else:
-        return dumme_multiplikation(a-1,b-1)+a+b-1
+        return dumme_multiplikation(a-1, b) + b
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
