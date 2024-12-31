cb396367639ae825c25eba39fd98552d17061b5c
stsi-ws24-12 <phammy98431@th-nuernberg.de>
2024-12-31 17:19:32 +0100

    Update Hausaufgabe.py

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -2,14 +2,12 @@ from typing import List
 
 
 def dumme_multiplikation(a:int, b:int):
-    if a == 0 or b == 0: # Multiplikation mit 0
-        return 0
     if a == 1:
         return b
-    if b == 1:
-        return a
+    if b ==1:
+        return a+1
     else:
-        return a + dumme_multiplikation(a, b-1) # Rekursive Multiplikation
+        return dumme_multiplikation(a-1,b-1)+a+b-1
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
