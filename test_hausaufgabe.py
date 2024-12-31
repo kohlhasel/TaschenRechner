ec83d48387680347f7e300ad8b0291ca2c99389b
T4M-W <tim.wagner0507@gmail.com>
2024-12-31 13:24:18 +0100

    Anpassung des Fehlers in TestAufgabeZwei

--- test_hausaufgabe.py
+++ test_hausaufgabe.py
@@ -83,7 +83,7 @@ class TestAufgabeZwei():
         #Act
         result = groesste_n_zahlen_implementation_1(liste,5)
         #Assert
-        assert set(result) == [6,7,8,9,10]
+        assert set(result) == {6, 7, 8, 9, 10}
 
 
     def test_findet_hoechste_5_implementation_2(self):
@@ -92,5 +92,5 @@ class TestAufgabeZwei():
         #Act
         result = groesste_n_zahlen_implementation_2(liste,5)
         #Assert
-        assert set(result) == [6,7,8,9,10]
+        assert set(result) == {6, 7, 8, 9, 10}
 
