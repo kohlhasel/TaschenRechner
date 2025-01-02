f8229a692b91a25a8af123a7b85278c87f7a7022
CarolinReuter <reuterca94725@th-nuernberg.de>
2025-01-02 11:05:49 +0100

    Update Hausaufgabe.py

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -2,12 +2,11 @@ from typing import List
 
 
 def dumme_multiplikation(a:int, b:int):
-    if a == 1:
+    if a == 0 or b == 0: # Multiplikation mit 0 ergibt 0
+        return 0
+    if a == 1:            # Multiplikation mit 1 ergibt b
         return b
-    if b ==1:
-        return a+1
-    else:
-        return dumme_multiplikation(a-1,b-1)+a+b-1
+    return b + dumme_multiplikation(a -1, b) #rekrusive Addition
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
