af08cfd6b461a1e704e56ca426c04bfbf7e75a07
SabrinaSchn <schneidersa80139@th-nuernberg.de>
2025-01-06 12:20:13 +0100

    Update Hausaufgabe.py

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -9,7 +9,7 @@ def dumme_multiplikation(a:int, b:int):
     if b ==1:
         return a
     else:
-        return dumme_multiplikation(a-1,b-1)+a+b-1
+        return dumme_multiplikation(a-1,b)+b
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
