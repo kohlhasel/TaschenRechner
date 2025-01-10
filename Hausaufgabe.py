017d5eca1275baca4aff75e804610032294af926
SabrinaSchn <schneidersa80139@th-nuernberg.de>
2025-01-10 15:16:57 +0100

    Update Hausaufgabe.py
    Ursprungsform der Rekursion herbeigeführt

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -6,10 +6,10 @@ def dumme_multiplikation(a:int, b:int):
         return 0
     if a == 1:
         return b
-    if b ==1:
+    if b == 1:
         return a
     else:
-        return dumme_multiplikation(a-1,b)+b
+        return dumme_multiplikation((a-1,b-1)+a+b-1
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
