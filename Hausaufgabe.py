bd8152a8ba446a35922f2af00b406c4249b438a3
KliMi-00 <klingermi96017@th-nuernberg.de>
2025-01-10 15:25:07 +0100

    Update Hausaufgabe.py
    Rekursion korrigieren + Multiplikation mit 0

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -1,13 +1,15 @@
 from typing import List
 
 
-def dumme_multiplikation(a:int, b:int):
+def dumme_multiplikation(a: int, b: int):
+    if a == 0 or b == 0:
+        return 0  # Multiplikation mit 0 ergibt 0
     if a == 1:
-        return b
-    if b ==1:
-        return a+1
+        return b  # Multiplikation mit 1 ergibt b
+    if b == 1:
+        return a  # Multiplikation mit 1 ergibt a
     else:
-        return dumme_multiplikation(a-1,b-1)+a+b-1
+        return a + dumme_multiplikation(a, b - 1)
 
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
