c9fa0877756107b3e09fe7a769cbae5ebbe3d012
jolenegs <gschnitzerjo103097@th-nuernberg.de>
2025-11-19 19:39:27 +0100

    Fehler in flaskus.py behoben

--- flaskus.py
+++ flaskus.py
@@ -20,7 +20,7 @@ APP = create_app()
 
 @APP.route('/version')
 def version_request():
-    return {"version": "4.0 -- Lukas Kohlhase"}
+    return {"version": "7.0 -- Jolene Gschnitzer"}
 
 @APP.route('/add', methods=['POST'])
 def addition_request():
