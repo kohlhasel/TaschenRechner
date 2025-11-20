ddb2ebc69911ce1bfe8112bf47d875056f469c87
nimataherheydari <taherheydarini103001@th-nuernberg.de>
2025-11-20 11:39:12 +0100

    Update version and fix subtraction function name

--- flaskus.py
+++ flaskus.py
@@ -20,7 +20,7 @@ APP = create_app()
 
 @APP.route('/version')
 def version_request():
-    return {"version": "4.1 -- Lukas Kohlhase"}
+    return {"version": "4.1 -- Nima Taherheydari"}
 
 @APP.route('/add', methods=['POST'])
 def addition_request():
@@ -34,7 +34,7 @@ def division_request():
 @APP.route('/sub', methods=['POST'])
 def sub_request():
     requestinfos={key: value for key,value in flask.request.json.items()}
-    return str(rechner.substraction(requestinfos['wert1'],requestinfos['wert2']))
+    return str(rechner.subtraktion(requestinfos['wert1'],requestinfos['wert2']))
 
 @APP.route('/mul', methods=['POST'])
 def mul_request():
