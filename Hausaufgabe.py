249340b1a418a90c91fff93d3670574d24221a79
KatharinaDeinhard <168655214+KatharinaDeinhard@users.noreply.github.com>
2025-12-22 10:02:59 +0100

    Fix: sort order in groesste_n_zahlen_implementation_2 corrected

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -12,5 +12,5 @@ def dumme_multiplikation(a: int, b: int):
 def groesste_n_zahlen_implementation_1(liste:List[int], n:int):
     return sorted(liste)[-n:]
 
-def groesste_n_zahlen_implementation_2(liste:List[int], n: int):
-    return sorted(liste, reverse=True)[:n]
+def groesste_n_zahlen_implementation_2(liste: List[int], n: int):
+    return sorted(liste)[-n:]
