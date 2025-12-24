c0b1cf39558eb08bb1b37802d97650efaf6e5dc1
jolenegs <gschnitzerjo103097@th-nuernberg.de>
2025-12-24 18:46:42 +0100

    Update test_hausaufgabe.py

--- test_hausaufgabe.py
+++ test_hausaufgabe.py
@@ -15,7 +15,15 @@ class TestAufgabeEins():
         # Assert
         assert result == 30
 
-
+     def test_multiplikation_mit_eins_rechts(self):
+        '''Multiplikation mit 1 als rechtem Operand'''
+        # Arrange
+        input1 = 5
+        input2 = 1
+        # Act
+        result = dumme_multiplikation(input1, input2)
+        # Assert
+        assert result == 5
 
     def test_assoziativ(self):
         '''Multiplikation ist assoziativ <==> (a*b)*c= a*(b*c)'''
