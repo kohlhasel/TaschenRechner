ff0a2c0800c070f2249836a7bdf6921091febb2c
gashial1 <gashial85078@th-nuernberg.de>
2026-01-09 00:32:23 +0100

    Update test_hausaufgabe.py

--- test_hausaufgabe.py
+++ test_hausaufgabe.py
@@ -15,6 +15,15 @@ class TestAufgabeEins():
         # Assert
         assert result == 30
 
+    def test_happy_path_one(self):
+        '''Testet, ob die Multiplikation korrekt ist, wenn der zweite Parameter 1 ist'''
+        # Arrange
+        input1 = 5
+        input2 = 1
+        # Act
+        result = dumme_multiplikation(input1, input2)
+        # Assert
+        assert result == 5
 
     def test_assoziativ(self):
         '''Multiplikation ist assoziativ <==> (a*b)*c= a*(b*c)'''
@@ -32,15 +41,6 @@ class TestAufgabeEins():
         input = 5
         # Act
         result = dumme_multiplikation(1, input)
-        # Assert
-        assert result == input
-
-      def test_1_neutrales_element_rechts(self):
-        '''1 ist neutrales Element der multiplikation <==> a*1=a '''
-        # Arrange
-        input = 5
-        # Act
-        result = dumme_multiplikation(input, 1)
         # Assert
         assert result == input
 
@@ -70,7 +70,7 @@ class TestAufgabeZwei():
         #Act
         result = groesste_n_zahlen_implementation_1(liste,5)
         #Assert
-        assert result == [6,7,8,9,10]
+        assert sorted(result) == [6, 7, 8, 9, 10]
 
 
     def test_findet_hoechste_5_implementation_2(self):
