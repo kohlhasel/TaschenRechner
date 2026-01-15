d56b687c1b41470aa75fbb44457a2fc11a4a31c2
uelpenichsa <151179753+uelpenichsa@users.noreply.github.com>
2026-01-15 10:10:02 +0100

    Update test_hausaufgabe.py

--- test_hausaufgabe.py
+++ test_hausaufgabe.py
@@ -71,5 +71,4 @@ class TestAufgabeZwei():
         #Act
         result = groesste_n_zahlen_implementation_2(liste,5)
         #Assert
-        assert result == [6,7,8,9,10]
-
+        assert set(result) == set(6,7,8,9,10)
